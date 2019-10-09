9206806639d492871fbaea977128b89ce65fdd44
yibincor <424024418@qq.com>
2019-10-09 17:31:38 +0800

    修改状态0、1

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,17 +19,15 @@ namespace WindowsFormsApp1
         }
         private void Button1_Click(object sender, EventArgs e)
         {
-            string sql = "select * from T_SyncTable";
+            string sql = "select * from T_SyncTable where IsSync = 0";
             DataSet myds = Maticsoft.DBUtility.DbHelperSQL.Query(sql, null);
             string conn, sqll;
             conn = "Server=172.18.3.119;Database=SK_CKAlarm;user id=sa;password=security999;";
-            for (int i = 0; i <= 2; i++)
-            {
-                object a = 1;
-                if (myds.Tables["ds"].Rows[i][3] != a)
-                {
-                Maticsoft.DBUtility.DbHelperSQL.ExecuteSql("update T_SyncTable set IsSync = 1");
-                } 
+            int Count = myds.Tables["ds"].Rows.Count;
+            for (int i = 1; i <= Count; i++)
+            {            
+                object b= myds.Tables["ds"].Rows[i-1][0];
+                Maticsoft.DBUtility.DbHelperSQL.ExecuteSql("update T_SyncTable set IsSync = 1 where id = " + b + "");
             }
         }            
     }
